b93ac74b4ee345ac7f86b74dd29a631f456927e8
caitlincorrigan <caitlinfcorrigan@gmail.com>
2023-12-06 12:46:15 -0800

    updates Friend model to includde absolute url

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Friend(models.Model):
@@ -9,3 +10,6 @@ class Friend(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'friend_id': self.id})
