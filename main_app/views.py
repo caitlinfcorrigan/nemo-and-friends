a1911d23bbed39c9508287ba7999575174a967bb
caitlincorrigan <caitlinfcorrigan@gmail.com>
2023-12-06 12:27:26 -0800

    creates CBV to create, update, and delete friends

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from .models import Friend
 
@@ -16,3 +17,15 @@ def friends_index(request):
 def friends_detail(request, friend_id):
     friend = Friend.objects.get(id=friend_id)
     return render(request, 'friends/detail.html', { 'friend': friend})
+
+class FriendCreate(CreateView):
+    model = Friend
+    fields = '__all__'
+
+class FriendUpdate(UpdateView):
+    model = Friend
+    fields = ['breed', 'age', 'habitat']
+
+class FriendDelete(DeleteView):
+    model = Friend
+    success_url = '/friends'
