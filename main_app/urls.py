8dfdc3efab293ebfd1cd18433822a774be7fa874
caitlincorrigan <caitlinfcorrigan@gmail.com>
2023-12-06 12:26:47 -0800

    creates routes to create, update, and delete friends

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('friends/', views.friends_index, name='index'),
     path('friends/<int:friend_id>/', views.friends_detail, name='detail'),
+    path('friends/create/', views.FriendCreate.as_view(), name='friends_create'),
+    path('friends/<int:pk>/update/', views.FriendUpdate.as_view(), name='friends_update'),
+    path('friends/<int:pk>/delete/', views.FriendDelete.as_view(), name='friends_delete'),
 ]
