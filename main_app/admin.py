044c1191e7f39552dce9fb5d0804ca8ced72186b
caitlincorrigan <caitlinfcorrigan@gmail.com>
2023-12-07 15:51:25 -0800

    updates base.html and amdin.py with Decor

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Friend, Feeding
+from .models import Friend, Feeding, Decor
 
 # Register your models here.
 admin.site.register(Friend)
 admin.site.register(Feeding)
+admin.site.register(Decor)
